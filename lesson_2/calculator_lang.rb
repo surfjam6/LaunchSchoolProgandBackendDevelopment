a301cfeaf2e90bcd3571619b45c4192bef11cca2
Jamie Weisbrod <weisbrod1@cox.net>
2015-12-22 23:34:46 +0000

    initial commit -JSW

---
+++ lesson_2/calculator_lang.rb
@@ -0,0 +1,29 @@
+# calculator_lang.rb
+##
+require 'yaml'
+
+## is MESSAGES considered a constant (all cap) or a hash ?
+MESSAGES = YAML.load_file('calculator_messages_lang.yml')
+#puts MESSAGES.inspect
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def valid_integer?(num)
+  (num.to_f.to_s == num) || (num.to_i.to_s == num)
+end
+
+def messages(message, lang='es')
+  MESSAGES[lang][message]
+end
+
+prompt(messages('welcome','es'))
+
+
+prompt(MESSAGES['es']['valid_name'])
+prompt(MESSAGES['en']['valid_name'])
+
+
+
+# prompt("Calculator exiting...")
