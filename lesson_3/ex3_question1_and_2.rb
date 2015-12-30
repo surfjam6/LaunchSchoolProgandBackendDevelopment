3dadd8117b049920c9c1bbcfffa890bd6b2b8244
Jamie Weisbrod <weisbrod1@cox.net>
2015-12-30 04:42:09 +0000

    Initial init

---
+++ lesson_3/ex3_question1_and_2.rb
@@ -0,0 +1,17 @@
+# ex3_question1_and_2.rb
+
+## question 1
+## Show an easier way to write this array:
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+## question 2
+## How can we add the family pet "Dino" to our usual array:
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones << "Dino"
+
+
+
