9a844b58899bef2dca77b29cecaa4a1dd007c30a
Jamie Weisbrod <weisbrod1@cox.net>
2016-01-08 18:49:42 +0000

    Refractored: added winner messages, fixed problem with simultaneous win when board is full

--- lesson_4/tic_tac_toe_display.rb
+++ lesson_4/tic_tac_toe_display.rb
@@ -36,19 +36,22 @@ end
 def empty_squares(brd)
   brd.keys.select {|num| brd[num] == EMPTY_SQUARE_MARKER}
 end
-# Refractored:
-# def board_full?(brd)
-#    if !brd.has_value?(EMPTY_SQUARE_MARKER)  # board has no blanks
-#      display_board(brd)
-#      prompt("GAME OVER!!")
-#      "true"
-#    else
-#      "false"
-#    end
-# end
 
-def board_full?(brd)
-  empty_squares(brd).empty?
+def display_result(winner)
+  if winner == 'X'
+    puts 'You won!!'
+  elsif winner == 'O'
+    puts "Computer wins!!"
+  elsif winner == 'board_full'
+    puts "Tie!!"
+  end
+end
+
+def board_full?(brd, won)
+  if empty_squares(brd).empty? && won != true # handles condition of win when board is also full
+    display_result('board_full')
+    return true
+  end
 end
 
 def good_user_entry!(brd, entry)
@@ -69,26 +72,13 @@ def enter_user_choice(brd)
   end
 end
 
-# Refractored after watching solution video:
-# def computer_entry!(brd)
-# choices = '123456789'
-# loop do
-#    computer_entry = choices[rand(choices.length)]
-#    # Is computer entry legal/possible ?
-#    if brd[computer_entry.to_i] == EMPTY_SQUARE_MARKER
-#      brd[computer_entry.to_i] = COMPUTER_MARKER
-#      break
-#    end
-#  end
-# end
-
 def computer_entry!(brd)
   computer_entry = empty_squares(brd).sample
   brd[computer_entry] = COMPUTER_MARKER
 end
 
 def return_winner(brd, marker) 
-  (brd[1] == marker && brd[2] == marker && brd[3] == marker) ||
+  win = (brd[1] == marker && brd[2] == marker && brd[3] == marker) ||
   (brd[4] == marker && brd[5] == marker && brd[6] == marker) ||
   (brd[7] == marker && brd[8] == marker && brd[9] == marker) ||
   (brd[1] == marker && brd[4] == marker && brd[7] == marker) ||
@@ -96,8 +86,13 @@ def return_winner(brd, marker)
   (brd[3] == marker && brd[6] == marker && brd[9] == marker) ||
   (brd[1] == marker && brd[5] == marker && brd[9] == marker) ||
   (brd[3] == marker && brd[5] == marker && brd[7] == marker)
+  if win 
+    display_result(marker)
+  end
+  return win
 end
 
+winner = ''
 board = initialize_board
 user_choice = ''
 board_numbers = {1=>"1", 2=>"2", 3=>"3", 4=>"4", 5=>"5", 6=>"6", 7=>"7", 8=>"8", 9=>"9"}
@@ -106,27 +101,27 @@ loop do # main
   # User play:
   enter_user_choice(board)
   display_board(board)
-#    prompt("User wins!!")
+  winner = return_winner(board, USER_MARKER)
 # Note: if you choose to play again, computer will go first.
-  if board_full?(board) || return_winner(board, USER_MARKER)
+  if board_full?(board, winner) || winner == true
+    ##   detect winner and print message
     prompt("Would you like to play again? (Y/N)")
     end_game = gets.chomp
     break if end_game.downcase != 'y'
     board = initialize_board
   end
+  
   # Computer play:
   computer_entry!(board)
   display_board(board)
-#  if return_winner(board, COMPUTER_MARKER)
-#    prompt("Computer wins!!")
-#  end
+  winner = return_winner(board, COMPUTER_MARKER)
 # Note: if you choose to play again, user will go first.
-  if board_full?(board) || return_winner(board, COMPUTER_MARKER)
+  if board_full?(board, winner) || winner == true
+    ##   detect winner and print message
     prompt("Would you like to play again? (Y/N)")
     end_game = gets.chomp
     break if end_game.downcase != 'y'
     board = initialize_board
-    display_board(board)
-
   end
+  display_board(board)
 end # main
